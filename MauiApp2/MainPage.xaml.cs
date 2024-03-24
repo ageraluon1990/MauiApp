cce2b787084e8056e88749d0732042e7c23aeabf
anton <antonkulagin27@gmail.com>
2024-03-24 11:25:46 +0200

    Add project files.

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MauiApp2.ViewModel;
+
+namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage(MainViewModel vm)
+        {
+            InitializeComponent();
+            BindingContext = vm;
+        }
+
+    }
+
+}
